147fa307a284ef27772e156e411bdeeebe3afb7c
jurajtoth <blackyno@gmail.com>
2016-11-17 15:55:56 +0100

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+mkdir target
+echo "Compiling project"
+javac -cp "libs/*" -d "target" sk/fei/ci/MovieList.java
+echo "Try to access http://localhost:4567/hello via your browser"
+java -cp "libs/*:./target" sk.fei.ci.MovieList
